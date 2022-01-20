c884abbe6460e1c8f21ecd5e9a2111a22ce399d5
lzhy <lzhy@DESKTOP-LKOQ9LE>
2022-01-20 16:48:36 +0800

    alc-0.0.1

--- src/core.h
+++ src/core.h
@@ -1,9 +1,9 @@
-#ifndef __TREE_H_
-#define __TREE_H_
+#ifndef __CORE_H_
+#define __CORE_H_
 #include"tool.h"
 
 void insert(Cell *c_list, rule *r);
 int match(Cell *c_list, message *m);
 void check(ACL_rules* r, message* m);
 
-#endif // !__TREE_H_
+#endif // !__CORE_H_
