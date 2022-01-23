29cc63cd9ffb5ff84d6b1001f6d35847122ff609
lzhy <lzhy@DESKTOP-LKOQ9LE>
2022-01-23 22:00:57 +0800

    update

--- src/core.c
+++ src/core.c
@@ -10,24 +10,7 @@ void insert(Cell* c_list, rule* r)
 	_d.destination_mask = (unsigned short)p->destination_mask;
 
 	unsigned int s_mask = (unsigned int)(p->source_mask >> 3);
-	if ((unsigned int)p->protocol[0] == 0)c_id[0] = PROTO_END_CELL;
-	else {
-		switch ((unsigned int)p->protocol[1])
-		{
-		case TCP:
-			c_id[0] = 0;
-			break;
-		case ICMP:
-			c_id[0] = 1;
-			break;
-		case UDP:
-			c_id[0] = 2;
-			break;
-		default:
-			fprintf(stderr, "Rule %d Error - unknown message protocol %u !\n", p->PRI, p->protocol[1]);
-			return;
-		}
-	}
+	
 	switch (s_mask)
 	{
 	case 0:
@@ -54,9 +37,29 @@ void insert(Cell* c_list, rule* r)
 	if (p->destination_port[0] == p->destination_port[1])c_id[PORT_LAYER] = p->destination_port[0] >> PORT_WIDTH;
 	else if(p->destination_port[0] >> PORT_WIDTH == p->destination_port[1] >> PORT_WIDTH)c_id[PORT_LAYER] = p->destination_port[0] >> PORT_WIDTH;
 	else c_id[PORT_LAYER] = PORT_END_CELL;
+	if ((unsigned int)p->protocol[0] == 0)c_id[0] = PROTO_END_CELL;
+	else {
+		switch ((unsigned int)p->protocol[1])
+		{
+		case TCP:
+			c_id[PROTO_LAYER] = 0;
+			break;
+		case ICMP:
+			c_id[PROTO_LAYER] = 1;
+			break;
+		case UDP:
+			c_id[PROTO_LAYER] = 2;
+			break;
+		default:
+			fprintf(stderr, "Rule %d Error - unknown message protocol %u !\n", p->PRI, p->protocol[1]);
+			return;
+		}
+	}
 
-	int id = (((c_id[0] * IP_SIZE_1 + c_id[IP_LAYER_1]) * IP_SIZE_2 + c_id[IP_LAYER_2]) * IP_SIZE_3 + c_id[IP_LAYER_3]) * PORT_SIZE + c_id[PORT_LAYER];
-	add_data(c_list + id, &_d);
+	//int id = (((c_id[0] * LAYER_1 + c_id[1]) * LAYER_2 + c_id[2]) * LAYER_3 + c_id[3]) * LAYER_4 + c_id[4];
+	//printf("%d %d\n", p->PRI, id);
+
+	add_data(c_list + (((c_id[0] * LAYER_1 + c_id[1]) * LAYER_2 + c_id[2]) * LAYER_3 + c_id[3]) * LAYER_4 + c_id[4], &_d);
 }
 
 
@@ -76,20 +79,20 @@ int match(Cell* c_list, message* m)
 	switch ((unsigned int)p->protocol)
 	{
 	case TCP:
-		c_id[0][0] = 0;
+		c_id[PROTO_LAYER][0] = 0;
 		break;
 	case ICMP:
-		c_id[0][0] = 1;
+		c_id[PROTO_LAYER][0] = 1;
 		break;
 	case UDP:
-		c_id[0][0] = 2;
+		c_id[PROTO_LAYER][0] = 2;
 		break;
 	default:
 		//fprintf(stderr, "Message Error - unknown message protocol %u!\n", e_protocol);
-		c_id[0][0] = PROTO_END_CELL;
+		c_id[PROTO_LAYER][0] = PROTO_END_CELL;
 		break;
 	}
-	c_id[0][1] = PROTO_END_CELL;
+	c_id[PROTO_LAYER][1] = PROTO_END_CELL;
 	c_id[IP_LAYER_1][0] = p->source_ip[3] >> IP_WIDTH_1;
 	c_id[IP_LAYER_1][1] = IP_EDN_CELL_1;
 	c_id[IP_LAYER_2][0] = p->source_ip[2] >> IP_WIDTH_2;
@@ -101,15 +104,15 @@ int match(Cell* c_list, message* m)
 
 	int res = 0x7FFFFFFF;
 	for (int i = 0; i < 2; i++) {
-		int id_1 = c_id[0][i] * IP_SIZE_1; // next layer start id
+		int id_1 = c_id[0][i] * LAYER_1; // next layer start id
 		for (int j = 0; j < 2; j++) {
-			int id_2 = (id_1 + c_id[IP_LAYER_1][j]) * IP_SIZE_2;
+			int id_2 = (id_1 + c_id[1][j]) * LAYER_2;
 			for (int k = 0; k < 2; k++) {
-				int id_3 = (id_2 + c_id[IP_LAYER_2][k]) * IP_SIZE_3;
+				int id_3 = (id_2 + c_id[2][k]) * LAYER_3;
 				for (int v = 0; v < 2; v++) {
-					int id_4 = (id_3 + c_id[IP_LAYER_3][v]) * PORT_SIZE;
+					int id_4 = (id_3 + c_id[3][v]) * LAYER_4;
 					for (int w = 0; w < 2; w++) {
-						int tar_id = id_4 + c_id[PORT_LAYER][w]; // the target cell id
+						int tar_id = id_4 + c_id[4][w]; // the target cell id
 						int _size = _c[tar_id].size;
 						if (_size == 0)continue;
 						data* _list = _c[tar_id].list;
@@ -158,20 +161,20 @@ int match_with_log(Cell* c_list, message* m, int *_cycle)
 	switch ((unsigned int)p->protocol)
 	{
 	case TCP:
-		c_id[0][0] = 0;
+		c_id[PROTO_LAYER][0] = 0;
 		break;
 	case ICMP:
-		c_id[0][0] = 1;
+		c_id[PROTO_LAYER][0] = 1;
 		break;
 	case UDP:
-		c_id[0][0] = 2;
+		c_id[PROTO_LAYER][0] = 2;
 		break;
 	default:
 		//fprintf(stderr, "Message Error - unknown message protocol %u!\n", e_protocol);
-		c_id[0][0] = PROTO_END_CELL;
+		c_id[PROTO_LAYER][0] = PROTO_END_CELL;
 		break;
 	}
-	c_id[0][1] = PROTO_END_CELL;
+	c_id[PROTO_LAYER][1] = PROTO_END_CELL;
 	c_id[IP_LAYER_1][0] = p->source_ip[3] >> IP_WIDTH_1;
 	c_id[IP_LAYER_1][1] = IP_EDN_CELL_1;
 	c_id[IP_LAYER_2][0] = p->source_ip[2] >> IP_WIDTH_2;
@@ -183,15 +186,15 @@ int match_with_log(Cell* c_list, message* m, int *_cycle)
 
 	int res = 0x7FFFFFFF;
 	for (int i = 0; i < 2; i++) {
-		int id_1 = c_id[0][i] * IP_SIZE_1; // next layer start id
+		int id_1 = c_id[0][i] * LAYER_1; // next layer start id
 		for (int j = 0; j < 2; j++) {
-			int id_2 = (id_1 + c_id[IP_LAYER_1][j]) * IP_SIZE_2;
+			int id_2 = (id_1 + c_id[1][j]) * LAYER_2;
 			for (int k = 0; k < 2; k++) {
-				int id_3 = (id_2 + c_id[IP_LAYER_2][k]) * IP_SIZE_3;
+				int id_3 = (id_2 + c_id[2][k]) * LAYER_3;
 				for (int v = 0; v < 2; v++) {
-					int id_4 = (id_3 + c_id[IP_LAYER_3][v]) * PORT_SIZE;
+					int id_4 = (id_3 + c_id[3][v]) * LAYER_4;
 					for (int w = 0; w < 2; w++) {
-						int tar_id = id_4 + c_id[PORT_LAYER][w]; // the target cell id
+						int tar_id = id_4 + c_id[4][w]; // the target cell id
 						int _size = _c[tar_id].size;
 						if (_size == 0)continue;
 						data* _list = _c[tar_id].list;
@@ -248,13 +251,13 @@ void analyse_log(ACL_rules* data)
 			switch ((unsigned int)p->protocol[1])
 			{
 			case TCP:
-				c_id[0] = 0;
+				c_id[PROTO_LAYER] = 0;
 				break;
 			case ICMP:
-				c_id[0] = 1;
+				c_id[PROTO_LAYER] = 1;
 				break;
 			case UDP:
-				c_id[0] = 2;
+				c_id[PROTO_LAYER] = 2;
 				break;
 			default:
 				fprintf(stderr, "Rule %d Error - unknown message protocol %u !\n", p->PRI, p->protocol[1]);
--- src/core.h
+++ src/core.h
@@ -4,28 +4,35 @@
 
 #define LEVEL 5
 
+#define PROTO_LAYER 4  // layer index
 #define PROTO_SIZE 4  // protocol layer cell number
 #define PROTO_END_CELL 3  // protocol layer cell end id
 
-#define IP_LAYER_1 1  // layer index
+#define IP_LAYER_1 0
 #define IP_SIZE_1 9
 #define IP_EDN_CELL_1 8
 #define IP_WIDTH_1 5  // using to comput index cell id ( x >> *_WIDYH = id )
 
-#define IP_LAYER_2 2
+#define IP_LAYER_2 1
 #define IP_SIZE_2 257
 #define IP_EDN_CELL_2 256
 #define IP_WIDTH_2 0
 
-#define IP_LAYER_3 3
+#define IP_LAYER_3 2
 #define IP_SIZE_3 9
 #define IP_EDN_CELL_3 8
 #define IP_WIDTH_3 5
 
-#define PORT_LAYER 4
+#define PORT_LAYER 3
 #define PORT_SIZE 65
 #define PORT_END_CELL 64
 #define PORT_WIDTH 10
+
+#define LAYER_0 IP_SIZE_1
+#define LAYER_1 IP_SIZE_2
+#define LAYER_2 IP_SIZE_3
+#define LAYER_3 PORT_SIZE
+#define LAYER_4 PROTO_SIZE
 
 #define CELL_SIZE 5412420
 
